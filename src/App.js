28e2d773b30bdf7013b6fddc34753a1a6c8dbefd
Lejla Spahić <lejla.spahic@edu.fit.ba>
2021-11-19 00:34:34 +0100

    Removing background, adding particles instead

--- src/App.js
+++ src/App.js
@@ -4,16 +4,123 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-tsparticles';
+
 
 import 'tachyons';
 
+const particlesOptions={
+  particles: {
+  number: {
+   value:30,
+   density:{
+     enable:true,
+      value_area:800
+    }
+  }
+}
+};
+
+const options2={
+  
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 120,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 1.5,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 140,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+}
+
+
 class  App extends Component {
+ 
   render(){
+    const particlesInit = (main) => {
+      console.log(main);
+  
+      // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    };
+  
+    const particlesLoaded = (container) => {
+      console.log(container);
+    };
+
   return (
     <div className="App">
+       <Particles id="tsparticles" className="particels" init={particlesInit}
+      loaded={particlesLoaded}
+                options={options2} />
     <Navigation/>
     <Logo/>
+
+    <Rank/>
     <ImageLinkForm/>
+    
     {/* 
     
      <FaceRecognition/>
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -9,7 +9,7 @@ const Logo=()=>{
         <div className="ma4 mt0">
             <Tilty className="tilty br2 shadow-2" glare scale={1.08} maxGlare={0.5}>
                 <div className="inner ">
-                    <img src={brain} alt="brain" />
+                    <img style={{paddingTop:'5x'}} src={brain} alt="brain" />
                     </div>
             </Tilty>
         </div>   
